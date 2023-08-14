32f21cfe7c94f997e56fb98e3eb619596faf8957
Ernest Sebastian <42617594+XeBastian@users.noreply.github.com>
2023-08-14 23:17:32 +0200

    Added int_declarations.c
    Interger declarations and initializations

---
+++ int_declarations.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+//   integer declaration and initializations
+    int age;
+    age = 90;
+    // The above steps can also be rewritten as int age = 90;
+    printf("%d", age);
+    return 0;
+}
