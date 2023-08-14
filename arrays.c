49933308c2038d1a3f29fc751f0f435826f08770
Ernest Sebastian <42617594+XeBastian@users.noreply.github.com>
2023-08-14 23:58:04 +0200

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int primeNumbers[2] ={1,2}; 
+    // basic array declarations above
+    // looping through an array
+    for (int i = 0; i <= 2; i++){
+        printf("The current iteration has %d as its number\n", i);
+    }
+    return 0;
+}
